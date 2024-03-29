c5d93655c6d3cd25cb341eddf2034840bac23b47
Petr Plsek <me@kalanys.com>
2024-03-29 09:45:42 +0100

    RedisWrapper - dependency analyzer 2

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -14,6 +14,7 @@ return $config
     // ignore errors on specific packages and paths
     ->ignoreErrorsOnPackageAndPath('alex-kalanis/kw_storage', __DIR__ . '/src/RedisStorage.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
     ->ignoreErrorsOnPackageAndPath('alex-kalanis/kw_storage', __DIR__ . '/src/PredisStorage.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
+    ->ignoreErrorsOnPackageAndPath('predis/predis', __DIR__ . '/src/PredisStorage.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
     ->ignoreErrorsOnPackageAndPath('predis/predis', __DIR__ . '/src/PredisWrapper.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
     ->ignoreErrorsOnPackageAndPath('predis/predis', __DIR__ . '/src/PredisWrapper/TPredis.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
 ;
